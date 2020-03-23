81ecec8b54a88be03b5d3a3ca8b09a691e6d7d68
sky <newbieskyy@gmail.com>
2020-03-23 23:51:11 +0900

    cloud config client test2

--- src/main/java/sky/project/skyproject1/controller/TestController.java
+++ src/main/java/sky/project/skyproject1/controller/TestController.java
@@ -1,6 +1,8 @@
 package sky.project.skyproject1.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
